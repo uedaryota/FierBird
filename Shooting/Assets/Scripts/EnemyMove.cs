9fb4b15fd95f4cf73bb5dd7b8c18ca3a34d6d74f
uedaryota <mr.335186@gmail.com>
2019-06-18 11:50:35 +0900

    EnemyMove追加

---
+++ Shooting/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    //Rigidbodyを変数に入れる
+    Rigidbody rb;
+    public float speed = 0.01f;//移動スピード
+    public float x = 0.0f;
+    public float z = 20.0f;
+
+    void Start()
+    {
+        //Rigidbodyを取得
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        rb.MovePosition(transform.position + new Vector3(x, 0, z) * -speed);
+    }
+}

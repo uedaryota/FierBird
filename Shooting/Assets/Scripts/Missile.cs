4d6cf19ba07c820a7b8f36b98b66cc0b53b6d3d1
masamx2 <masatakamx2@gmail.com>
2019-06-21 11:30:00 +0900

    ミサイル追加

---
+++ Shooting/Assets/Scripts/Missile.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public float range = 100f;
+    public int damagePerShot = 20;
+    public float timeBetweenBullets = 0.15f;
+
+    float timer;
+    private float rad;
+    public GameObject missile;
+    public Transform muzzle;
+
+    private Vector3 Position;
+
+    public float Speed;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (Input.GetKey(KeyCode.X) && timer >= timeBetweenBullets && Time.timeScale != 0)
+        {
+            Shot();
+        }
+    }
+    void Shot()
+    {
+        timer = 0f;
+        //弾丸の複製
+        GameObject bullets = Instantiate(missile) as GameObject;
+
+        //弾丸の位置を調整
+        bullets.transform.position = muzzle.position;
+
+        
+    }
+}

7dbce8ba4df76d7902f2592a2bc6533c52716a55
uedaryota <mr.335186@gmail.com>
2019-06-21 16:13:56 +0900

    なんか色々

---
+++ Shooting/Assets/Scripts/Bullet_Destry.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Destry : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Enemy" || col.gameObject.tag == "Death" || col.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}

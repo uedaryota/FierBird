f1444660ee2e17e19ad9ab797d040efa30de642a
uedaryota <mr.335186@gmail.com>
2019-06-25 10:16:03 +0900

    一時保存

--- Shooting/Assets/Scripts/PlayerHealth.cs
+++ Shooting/Assets/Scripts/PlayerHealth.cs
@@ -8,23 +8,14 @@ public class PlayerHealth : MonoBehaviour
     public int currentHealth;
     public Slider healthSlider;
     public Image damageImage;
-    public AudioClip deathClip;
     public float flashSpeed = 5f;
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
-    Animator anim;
-    AudioSource playerAudio;
-    //PlayerMovement playerMovement;
-    //PlayerShooting playerShooting;
     bool isDead;
     bool damaged;
     public int Heel = 20;
     //各コンポーネントを参照します。
     void Awake()
     {
-        anim = GetComponent<Animator>();
-        playerAudio = GetComponent<AudioSource>();
-        //playerMovement = GetComponent<PlayerMovement>();
-        //playerShooting = GetComponentInChildren<PlayerShooting>();
         currentHealth = startingHealth;
     }
     //ダメージ受けたら赤く画面が光る
@@ -48,7 +39,6 @@ public class PlayerHealth : MonoBehaviour
         damaged = true;
         currentHealth -= amount;
         healthSlider.value = currentHealth;
-        playerAudio.Play();
         if (currentHealth <= 0 && !isDead)
         {
             Death();
@@ -57,12 +47,6 @@ public class PlayerHealth : MonoBehaviour
     void Death()
     {
         isDead = true;
-        //playerShooting.DisableEffects();
-        anim.SetTrigger("Die");
-        playerAudio.clip = deathClip;
-        playerAudio.Play();
-        //playerMovement.enabled = false;
-        //playerShooting.enabled = false;
     }
     public void RestartLevel()
     {
